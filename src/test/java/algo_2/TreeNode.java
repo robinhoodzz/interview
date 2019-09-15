54ea522663e777be6b3cb09695c04c743489f03e
robin <robinhoodzz@126.com>
2019-09-15 23:00:55 +0800

    家里练习

---
+++ src/test/java/algo_2/TreeNode.java
@@ -0,0 +1,16 @@
+package algo_2;
+
+/**
+ * Created by Administrator on 2019/9/14.
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+
+}
